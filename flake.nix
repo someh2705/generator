8b262f773d73d5289e37268b53387c5333083c4b
someh2705 <someh2705@gmail.com>
2025-11-01 20:10:39 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "capstone project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        unstable = import nixpkgs-unstable { inherit system; };
+
+        devTools = with unstable; [
+          uv
+          ty
+          ruff
+          pyright
+          watchexec
+        ];
+
+        python = unstable.python3.withPackages (ps: with ps; [
+          addict
+          networkx
+          icecream
+
+          python-lsp-server
+          python-lsp-ruff
+        ]);
+
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = devTools;
+          buildInputs = [ python ];
+
+          shellHook = ''
+            export PYTHONPATH=$PYTHONPATH:${python}/${python.sitePackages}
+          '';
+        };
+      });
+}
